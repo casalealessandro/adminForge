74971730ae46ccd26da45df150642552744b875d
Alessandro Casale <81802080+casalealessandro@users.noreply.github.com>
2026-09-14 15:06:30 +0200

    fix(data-grid): anchor action menu to trigger

--- src/app/core/data-grid/td-item/td-item.component.ts
+++ src/app/core/data-grid/td-item/td-item.component.ts
@@ -555,14 +555,6 @@ export class TdItemComponent {
       top: rect.bottom + window.scrollY, // Posiziona l'overlay sotto il bottone
       left: rect.left + window.scrollX
     };
-    // Calcola la larghezza della finestra e controlla che l'overlay non esca dal lato destro
-    const windowWidth = window.innerWidth;
-    const overlayWidth = 423; // Imposta la larghezza dell'overlay (può essere dinamica se necessario)
-
-    // Se l'overlay va fuori dalla finestra, correggiamo la posizione
-    if (position.left + overlayWidth > windowWidth) {
-      position.left = windowWidth - overlayWidth - 10; // Imposta un piccolo margine
-    }
 
     const data = {
       position: position,
